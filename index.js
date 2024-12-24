ed253886f5ff65202cfb0942f5ecbda8f01feb96
Hari Krishnan <harikrishnan.k.a.s@gmail.com>
2024-12-24 23:25:50 +0530

    almostDone

--- index.js
+++ index.js
@@ -17,3 +17,44 @@ function onClick(){
 
 
 document.addEventListener("click",onClick);
+
+
+
+function reveal() {
+    
+    var reveals = document.querySelectorAll(".container",".reveal");
+    for (var i = 1; i < reveals.length; i++) {
+      var prev = reveals[i-1].getBoundingClientRect().top;
+      if (prev < 100) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+let imgLen = 0;
+var positionx = new Array();
+var positiony = new Array();
+var j = 0;
+for(var i = 20; i >= 5; i=i-0.25) {
+  positiony[j] = 40-Math.sqrt(80*i) ;
+  positionx[j] = i;
+  j++;
+}
+
+  function trackScrollPosition() {
+    const y = window.scrollY;
+    const label = Math.min(Math.floor(y/14)+1, imgLen++);
+    let position = positionx[label] + "vw " +positiony[label] +"vh";
+    document.getElementById("star_img").style.translate = position;
+    
+  }
+  
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    addEventListener("scroll", (event) => {trackScrollPosition();});
+  });
